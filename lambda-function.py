88151ac8a360676c178e155427972b50684f2b0f
Ahmar Jenkins <104280394+Jstarri@users.noreply.github.com>
2022-11-11 14:43:12 -0500

    Update lambda-function.py

--- lambda-function.py
+++ lambda-function.py
@@ -1,9 +1,17 @@
 import boto3
+import json
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
-
-    # Print out bucket names
-
-    for bucket in s3.buckets.all():
-        return(bucket.name)
+    s3 = boto3.client('s3')
+    response = s3.list_buckets()
+    
+    bucket_names = []
+    buckets = response["Buckets"]
+    
+    for bucket in buckets:
+        print(bucket["Name"])
+        bucket_names.append(bucket["Name"])
+        
+    return {
+        "body": json.dumps(bucket_names)
+    }
